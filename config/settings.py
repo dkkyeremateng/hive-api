c2f1419a5cfc18607aba232503183bf392a764c6
Daniel Kofi Kyeremateng <kofwayne305@gmail.com>
2018-08-16 09:59:51 +0000

    Initial Commit

---
+++ config/settings.py
@@ -0,0 +1,7 @@
+# Mongodb Database
+DB = 'Your database name'
+
+MONGODB_DB = DB
+
+
+SECRET_KEY = 'Your secret key'
---
+++ instance/settings.py
@@ -0,0 +1,7 @@
+# Flask Config
+SECRET_KEY = 'hello-secret-key'
+
+# Mongodb Database
+DB = 'hive-site'
+
+MONGODB_DB = DB
